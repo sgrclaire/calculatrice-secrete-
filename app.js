ca16f395ee2dad6cbec3a2d1bca636145c7a9990
sgrclaire <sgr.claire@gmail.com>
2025-10-05 18:40:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+const display = document.getElementById('display');
+const keys = document.querySelector('.keys');
+let expr = "";
+keys.addEventListener('click', e=>{
+  if(!e.target.matches('button')) return;
+  let t = e.target.textContent;
+  if(t=="C") expr=""; else if(t=="=") expr=eval(expr); else expr+=t;
+  display.textContent = expr || "0";
+});
